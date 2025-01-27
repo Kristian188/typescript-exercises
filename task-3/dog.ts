ec31d5cf6a6ea47ac7cff09ad7f18a872a66edf5
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-01-27 23:53:16 +0200

    dog class

---
+++ task-3/dog.ts
@@ -0,0 +1,25 @@
+import { Animal } from './animal';
+
+export class Dog extends Animal {
+    private breed: string;
+
+    constructor(name: string, age: number, breed: string) {
+        super(name, age);
+        this.breed = breed;
+    }
+    
+    public get Breed(): string{
+        return this.breed;
+    }
+
+    makeSound(): string{
+        
+        return super.makeSound() + "Bark!";
+    }
+
+
+    getInfo(): string{
+        return `${super.getInfo()}, Breed: ${this.breed}`;
+    }
+
+}
