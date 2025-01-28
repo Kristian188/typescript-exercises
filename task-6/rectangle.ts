4e4f7ee0260a410ed5ab7c43d6e1e3c880d21813
Kristian Palovski <Kristian_Palovski@abv.bg>
2025-01-28 20:55:00 +0200

    rectangle class

---
+++ task-6/rectangle.ts
@@ -0,0 +1,31 @@
+import { Shape } from './shape';
+
+export class Rectangle extends Shape {
+    private width: number;
+    private height: number;
+
+    constructor(width: number, height: number) {
+        super();
+        this.width = width;
+        this.height = height;
+    }
+
+    public get Width(): number {
+        return this.width;
+    }
+    public get Height(): number {
+        return this.height;
+    }
+
+    public area(): number {
+        return this.width * this.height;
+    }
+
+    public displayArea(): void {
+        console.log(`Rectangle ${super.printArea}`);
+    }
+
+
+    
+
+}
